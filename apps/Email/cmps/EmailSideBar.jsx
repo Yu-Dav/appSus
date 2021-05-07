7d4654fa653f9dd64e51b098952402aec0753dea
Noga Jacobi <nogajacobi@gmail.com>
2021-05-07 21:57:26 +0200

    icons

--- apps/Email/cmps/EmailSideBar.jsx
+++ apps/Email/cmps/EmailSideBar.jsx
@@ -2,10 +2,10 @@ export class EmailSideBar extends React.Component {
     render() {
         return (
             <div className="email-controls flex flex-column">
-                <button onClick={() => this.props.onOpenModal()}>compose</button>
-                <button onClick={() => this.props.onSetView('inbox')}>Inbox</button>
-                <button onClick={() => this.props.onSetView('trash')}>Trash</button>
-                <button onClick={() => this.props.onSetView('star')}>Starred</button>
+                <button className="btn compose-btn" onClick={() => this.props.onOpenModal()}>Compose</button>
+                <button className= "btn fa fas fa-inbox" onClick={() => this.props.onSetView('inbox')}>Inbox</button>
+                <button className= "btn fa fa-trash" onClick={() => this.props.onSetView('trash')}>Trash</button>
+                <button  className="btn fa fas fa-star" onClick={() => this.props.onSetView('star')}>Starred</button>
             </div>
         )
     }
