ad53504b3785038e0dba3c1cc5ced0023648a8bb
Noga Jacobi <nogajacobi@gmail.com>
2021-05-07 23:18:56 +0200

    sideBar design

--- apps/Email/cmps/EmailDetails.jsx
+++ apps/Email/cmps/EmailDetails.jsx
@@ -4,7 +4,7 @@ export function EmailDetails({email,onDeleteEmail}) {
     return (
         <Link to="/email">
         <div>
-            <i className="fa fa-trash" onClick={() => onDeleteEmail(email.id)}></i>
+            {/* <i className="fa fa-trash" onClick={() => onDeleteEmail(email.id)}></i> */}
             <Link to={`/keep?body=${email.body}`}>
                 <i>save as note</i>
             {/* /email/compose?subject=my note&body= note about the rain */}
--- apps/Email/cmps/EmailSideBar.jsx
+++ apps/Email/cmps/EmailSideBar.jsx
@@ -2,10 +2,12 @@ export class EmailSideBar extends React.Component {
     render() {
         return (
             <div className="email-controls flex flex-column">
-                <button className="btn compose-btn" onClick={() => this.props.onOpenModal()}>Compose</button>
+              
+                <button className="btn compose-btn" onClick={() => this.props.onOpenModal()}>  <img className="plus-sign" src="../../../assets/img/plus.png" alt=""></img>Compose</button>
                 <button className= "btn fa fas fa-inbox" onClick={() => this.props.onSetView('inbox')}>Inbox</button>
                 <button className= "btn fa fa-trash" onClick={() => this.props.onSetView('trash')}>Trash</button>
                 <button  className="btn fa fas fa-star" onClick={() => this.props.onSetView('star')}>Starred</button>
+               
             </div>
         )
     }
