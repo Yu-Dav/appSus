70df75fc4459aa4bdd6c806ac9d0fafcde313d4b
Noga Jacobi <nogajacobi@gmail.com>
2021-05-08 19:19:17 +0200

    small change

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -5,12 +5,7 @@ export class Home extends React.Component {
     render() {
         return (
             <section className="home-page container">
-<<<<<<< HEAD
-                <div className="hero">Welcome to your app</div>
-=======
                 <div className="hero">Welcome to your new favorite app</div>
-
->>>>>>> 3459820417279cf6178e62be1c356b0b9c23b9a8
                 <Link to="/keep">
                     <div className="keep">
                         <img src="../assets/img/home-keep.jpg" alt="" />
