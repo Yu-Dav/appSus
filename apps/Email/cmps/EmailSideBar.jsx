0447fb38c5d6db588a9c00a06a1c020a066303b3
Noga Jacobi <nogajacobi@gmail.com>
2021-05-08 10:50:21 +0200

    border issues email-list

--- apps/Email/cmps/EmailSideBar.jsx
+++ apps/Email/cmps/EmailSideBar.jsx
@@ -4,9 +4,9 @@ export class EmailSideBar extends React.Component {
             <div className="email-controls flex flex-column">
               
                 <button className="btn compose-btn" onClick={() => this.props.onOpenModal()}>  <img className="plus-sign" src="../../../assets/img/plus.png" alt=""></img>Compose</button>
-                <button className= "btn fa fas fa-inbox" onClick={() => this.props.onSetView('inbox')}>Inbox</button>
-                <button className= "btn fa fa-trash" onClick={() => this.props.onSetView('trash')}>Trash</button>
-                <button  className="btn fa fas fa-star" onClick={() => this.props.onSetView('star')}>Starred</button>
+                <button className= "btn fa fas fa-inbox-side" onClick={() => this.props.onSetView('inbox')}>Inbox</button>
+                <button className= "btn fa fa-trash-side" onClick={() => this.props.onSetView('trash')}>Trash</button>
+                <button  className="btn fa fas fa-star-side" onClick={() => this.props.onSetView('star')}>Starred</button>
                
             </div>
         )
